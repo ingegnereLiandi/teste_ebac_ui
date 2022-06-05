d20541a550448126ff92e7ac0f474e2194f096c3
Liandi <douglasliandi@hotmail.com>
2022-06-05 11:07:49 -0300

    alterações no teste automatizados

---
+++ cypress/e2e/produtos.spec.cy.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade página de produtos', () => {
+
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]').eq("2").click()
+        cy.get('.button-variable-item-34').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.single_add_to_cart_button').click()
+
+    });
+
+
+
+    it('Deve adicionar produto no carrinho', () => {
+        var quantidade = 90
+
+        cy.get('[class="product-block grid"]').eq("2").click()
+        cy.get('.button-variable-item-34').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + " × “Aether Gym Pant” ")
+
+
+    });
+
+  
+
+
+    });
+
+

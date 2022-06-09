99a12e0e4c092c44a5d41d6334981026825cca5b
Liandi <douglasliandi@hotmail.com>
2022-06-09 13:39:35 -0300

    criação pasta page-objects

--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+import EnderecoPage from '../support/page-objects/endereco.page.cy'
 
 describe('Funcionaidade endereços- Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -9,7 +10,8 @@ describe('Funcionaidade endereços- Faturamento e Entrega', () => {
        
     });
 
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-      
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+      EnderecoPage.editarEnderecoFaturamento('Eduardo', 'Silva', 'Google','Bósnia','Al: Rio Negro', '201','Osasco' ,'São Paulo', '1111111', '(021) 94942-2153','eduardo.lima@hotmail.com')
+      //cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
